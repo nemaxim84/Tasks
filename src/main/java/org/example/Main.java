117f20686a158365f20bb76b6f088d9a7b019bc1
nemaxim84 <memaxim@yandex.ru>
2025-09-01 18:20:53 +0300

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
+        }
+
+        int n = 9;   //n-ю цифру последовательности
+
+        List<Integer> list = new ArrayList<>();
+        for(int i = 1; list.size()<=n;i++)
+
+        {
+            List<Integer> listTmp = new ArrayList<>();
+            int a = i * i;
+            int tmp = a;
+            while (tmp > 0) {
+                listTmp.add(tmp % 10);
+                tmp = tmp / 10;
+            }
+            Collections.reverse(listTmp);
+            list.addAll(listTmp);
+        }
+
+        System.out.println(list.get(n-1));
+
+
+    }
+}
---
+++ src/main/java/org/example/Task1.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Task1 {
+
+
+}
