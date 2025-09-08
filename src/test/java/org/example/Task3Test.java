214fe968d844616bd2f5931ea582d8a8a96db0d3
nemaxim84 <memaxim@yandex.ru>
2025-09-08 19:14:01 +0300

    Task3 + test

--- src/test/java/org/example/Task3Test.java
+++ src/test/java/org/example/Task3Test.java
@@ -12,18 +12,22 @@ class Task3Test {
     void findLengthACBD() {
         Assertions.assertEquals(5, task3.findLength(1, 20, 15, 25));
     }
+
     @Test
     void findLengthCABD() {
         Assertions.assertEquals(5, task3.findLength(10, 15, 1, 20));
     }
+
     @Test
     void findLengthCADB() {
         Assertions.assertEquals(12, task3.findLength(12, 24, 1, 18));
     }
+
     @Test
     void findLengthABCD() {
         Assertions.assertEquals(-1, task3.findLength(1, 2, 10, 18));
     }
+
     @Test
     void findLengthCDAB() {
         Assertions.assertEquals(-1, task3.findLength(10, 12, 3, 4));
