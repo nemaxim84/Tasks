ae4ac6b9ac59fb88a6bc11209e29672b36d87cfa
nemaxim84 <memaxim@yandex.ru>
2025-09-08 18:57:40 +0300

    Task2 + test

--- src/test/java/org/example/Task2Test.java
+++ src/test/java/org/example/Task2Test.java
@@ -7,21 +7,25 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class Task2Test {
     Task2 task2 = new Task2();
+
     @Test
     void triangleProperty1() {
-        Assertions.assertEquals(1, task2.triangleProperty(3,4,5));
+        Assertions.assertEquals(1, task2.triangleProperty(3, 4, 5));
     }
+
     @Test
     void triangleProperty2() {
-        Assertions.assertEquals(2, task2.triangleProperty(9,6,5));
+        Assertions.assertEquals(2, task2.triangleProperty(9, 6, 5));
     }
+
     @Test
     void triangleProperty0() {
-        Assertions.assertEquals(0, task2.triangleProperty(13,14,15));
+        Assertions.assertEquals(0, task2.triangleProperty(13, 14, 15));
     }
+
     @Test
     void trianglePropertyImpossible() {
-        Assertions.assertEquals(-1, task2.triangleProperty(20,3,1));
+        Assertions.assertEquals(-1, task2.triangleProperty(20, 3, 1));
     }
 
 }
